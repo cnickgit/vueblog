bb4fa5dab7a4d4cc84fc5cd990da3aa78c44d3cf
wsg <1209930447@qq.com>
2020-11-25 21:28:59 +0800

    组件图片

--- vueblog-vue/src/App.vue
+++ vueblog-vue/src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
     <router-view/>
+    <tabbar></tabbar>
   </div>
 </template>
 
+<script>
+  import tabbar from './components/tabbar.vue';
+  export default {
+    name: 'App',
+    components: {
+      tabbar
+    }
+  }
+</script>
 <style>
   #app {
     max-width: 960px;
