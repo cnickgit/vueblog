b0f2117f8d89a4146f7336d3bfc2bdd23d37e664
wsg <1209930447@qq.com>
2020-11-29 20:44:28 +0800

    后端接口新增成功

--- vueblog-java/src/main/java/com/markerhub/controller/ZyjController.java
+++ vueblog-java/src/main/java/com/markerhub/controller/ZyjController.java
@@ -7,6 +7,7 @@ import com.markerhub.entity.ZyjToken;
 import com.markerhub.mapper.ZyjTokenMapper;
 import com.markerhub.util.TimeUtil;
 import com.markerhub.vo.AddTokenVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -18,10 +19,10 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 @RestController
+@Slf4j
 public class ZyjController {
 
     @Autowired
@@ -29,7 +30,6 @@ public class ZyjController {
 
     @GetMapping(value = "/addToken")
     public Result addZyjToken(@RequestParam("type") String type){
-//        @RequestBody AddTokenVo addTokenVo
         ZyjToken zyjToken = new ZyjToken();
         zyjToken.setId(UUID.randomUUID().toString());
         zyjToken.setType(type);
@@ -58,12 +58,19 @@ public class ZyjController {
             zyjToken.setMoney(25);
             zyjToken.setTypeRemarks("25元 200次 不限制时间");
         }
-        int i = zyjTokenMapper.insert(zyjToken);
-        if(i>0){
-            return Result.succ("新增成功");
-        }else{
+        int i = 0;
+        try {
+            i = zyjTokenMapper.insert(zyjToken);
+            if(i>0){
+                return Result.succ("新增成功");
+            }else{
+                return Result.fail("新增失败");
+            }
+        }catch (Exception e){
+            log.error("新增时发生异常,异常原因:--->",e.toString());
             return Result.fail("新增失败");
         }
+
     }
 
     @GetMapping(value = "/enableToken")
@@ -91,23 +98,31 @@ public class ZyjController {
 
         }else if(PcConstant.TYPE_TYEFIVE.equals(zyjToken.getType())){
         }
-        zyjTokenMapper.updateById(zyjToken);
-        return null;
+        int i=0;
+        try {
+            i = zyjTokenMapper.updateById(zyjToken);
+            if(i>0){
+                return Result.succ("token启动成功!");
+            }else{
+                return Result.succ("token启动失败!");
+            }
+        }catch (Exception e){
+            log.error("token启动失败,失败原因--->",e.toString());
+            return Result.succ("token启动失败,失败原因--->"+e.toString());
+        }
     }
 
     @GetMapping(value="/tokens")
     public Result getTokens(@RequestParam("enableType") String enableType){
-        List<ZyjToken> zyjTokens = zyjTokenMapper.queryAllTokens(enableType);
-        return Result.succ(zyjTokens);
-    }
+        List<ZyjToken> zyjTokens = null;
+        try {
+            zyjTokens = zyjTokenMapper.queryAllTokens(enableType);
+            return Result.succ(zyjTokens);
+        }catch (Exception e){
+            log.error("查询失败,失败原因:--->",e.toString());
+            return Result.fail("查询失败,失败原因:--->"+e.toString());
+        }
 
-
-    @GetMapping("checkJWT")
-    public Result checkJwt(String code){
-
-
-
-        return null;
     }
 
     @GetMapping(value = "/zyjLogin")
@@ -128,6 +143,23 @@ public class ZyjController {
         System.out.println(response.getHeaders().get("Set-Cookie").get(0));
         System.out.println(response.getBody());
         return response.getBody();
+    }
+
+    @GetMapping(value = "/myRecord")
+    public Result getMyRecord(){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://139.159.141.200/app/superscanPH/opQuery.jsp";
+        HttpHeaders headers = new HttpHeaders();
+        List<String> cookies =new ArrayList<String>();
+        /* 登录获取Cookie 这里是直接给Cookie，可使用下方的login方法拿到Cookie给入*/
+        cookies.add("UM_distinctid=1757d88999410f-02e5f5298d538d-c781f38-100200-1757d889995401; CNZZDATA4962612=cnzz_eid%3D1937187844-1606304311-http%253A%252F%252F139.159.141.200%252F%26ntime%3D1606304311; CNZZDATA1276815554=1646318526-1606304320-http%253A%252F%252F139.159.141.200%252F%7C1606304320; JSESSIONID=5E5F639D84A04222075D74BFCD4C9019; CNZZDATA1277894835=1541360565-1604126342-http%253A%252F%252F139.159.141.200%252F%7C1606476364");       //在 header 中存入cookies
+        headers.put(HttpHeaders.COOKIE,cookies);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+        map.add("m", "mine");
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity( url, request , String.class );
+        return Result.succ(response.getBody());
     }
 
     @GetMapping(value = "/search")
